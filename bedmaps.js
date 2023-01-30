1bc990e3372c7ca13d84195c96cfc06072b2111b
hsfa <94189135+catedro@users.noreply.github.com>
2023-01-30 19:14:46 +0100

    Update bedmaps.js

--- bedmaps.js
+++ bedmaps.js
@@ -3,7 +3,8 @@ document.write('hola');
 }
 
 function showTopBar(){
-  document.write(  
+  document.write('asi es');  
+    /*
     'div class="cout"><div class="c1"><label id="label"><input type="checkbox" Id="Todo" onclick="setAllSectors()" checked><b>Marcar Todo</b> </label><br/>'+
     '<label id="label"><input onclick="showDesktop()" type="checkbox" Id="huecos" name="huecos"><b>Camas libres</b> </label></div>'+
     '<div class="c2"><label><input onclick="Show()" type="checkbox" Id="0" name="1-0" checked>1-0</label>'+
@@ -18,6 +19,7 @@ function showTopBar(){
     '<label><input onclick="showDesktop()" type="checkbox" Id="9" name="4-B" checked>1-B</label>'+
     '</div><div class="c22" Id="S1"></div><div class="c23" Id="S2"></div><div class="c24" Id="S3"></div>'+
     '<div class="c25" Id="S4"></div><div class="c6" Id="Pac"></div><div class="c7" Id="Btn"></div></div>  <div Id="frm"></div>');
+*/
 }
 
 function showDesktop(){
